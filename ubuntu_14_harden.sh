4264fc128deb2003aaffc11f1594ab96dfaecf38
Ragna-Axe <147952264+Ragna-Axe@users.noreply.github.com>
2024-01-11 19:07:50 -0600

    Update ubuntu_14_harden.sh

--- ubuntu_14_harden.sh
+++ ubuntu_14_harden.sh
@@ -17,10 +17,10 @@ iptables -P FORWARD DROP
 iptables -P OUTPUT ACCEPT
 
 # Allow incoming SSH (replace 22 with your SSH port if it's different)
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+#iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 
 # Allow incoming HTTP (replace 80 with your HTTP port if it's different)
-#iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 
 # Allow incoming HTTPS (replace 443 with your HTTPS port if it's different)
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT

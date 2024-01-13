0fe154bcfad6281c08b705bab7372a529b3cb867
Ragna-Axe <147952264+Ragna-Axe@users.noreply.github.com>
2024-01-13 13:12:55 -0600

    Password change 18

---
+++ ubuntu_18_PWchange.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Remove all users except root
+for username in $(awk -F: '$3 >= 1000 && $1 != "root" {print $1}' /etc/passwd); do
+    userdel -r $username
+done
+
+# Change root password
+echo "root:GenuineGMparts!22" | chpasswd
+
+# Change password for other system accounts if needed
+# echo "username:new_password" | chpasswd
+
+echo "User removal and password change completed."

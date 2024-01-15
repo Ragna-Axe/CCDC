d011194ec1e3355dbc5828817060cd2c4164ae30
Ragna-Axe <147952264+Ragna-Axe@users.noreply.github.com>
2024-01-15 07:34:18 -0600

    Password reset ad accounts

---
+++ windows_password_Reset.ps1
@@ -0,0 +1,15 @@
+﻿# Written by: Bobby Dorman
+# Created on: 10/6/2020
+# Note: Dear god don't run this on anything production. 
+# Purpose: To get a list of users from AD and loop through user accounts to reset password. 
+
+# Get list of users: 
+$users = Get-ADUser -filter * -Properties SamAccountName
+
+# Set AD Account Passwords: 
+foreach($user in $users)
+    {
+        Set-ADAccountPassword -Identity $user.SamAccountName -Reset -NewPassword (ConvertTo-SecureString -AsPlainText "GenuineGMparts!22" -Force)
+        Write-Host "Password set for: " $user.SamAccountName
+    }
+

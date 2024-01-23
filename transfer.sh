7e5c72f14bdad8e751ae243743f3de8a2072cbfa
Ragna-Axe <147952264+Ragna-Axe@users.noreply.github.com>
2024-01-23 10:53:23 -0600

    Create transfer.sh

---
+++ transfer.sh
@@ -0,0 +1,11 @@
+#Upload using cURL
+curl --upload-file ./hello.txt https://transfer.sh/hello.txt
+https://transfer .sh/HdCWvzUyJV/hello.txt
+
+#Using the shell function
+transfer hello.txt
+####################################################### 100.0%
+https://transfer.sh/9WcHWcHHiu/hello.txt
+
+#Upload from web
+Drag your files here, or click to browser

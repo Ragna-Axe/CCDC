e1aa2a859371bee4b053790722b76de7f0deda72
Ragna-Axe <147952264+Ragna-Axe@users.noreply.github.com>
2024-01-23 19:48:37 -0600

    Add files via upload
    Debian ufw script to allow dns and ntp

---
+++ debian_firewall.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Ensure script is run with root privileges
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Please run the script as root."
+    exit 1
+fi
+
+# Close common ports
+ufw default deny incoming
+ufw default allow outgoing
+
+# Allow essential ports
+#ufw allow ssh
+ufw allow 53/udp   # DNS
+ufw allow 123/udp  # NTP
+
+# Additional rules based on your specific requirements
+# ufw allow <port>/<protocol>
+# Example: ufw allow 8080/tcp
+
+# Enable firewall
+ufw enable
+
+# Display firewall status
+ufw status

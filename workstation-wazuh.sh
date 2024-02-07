e9a6053b47d8295548e32a4f22731292ab3bbac0
Ragna-Axe <147952264+Ragna-Axe@users.noreply.github.com>
2024-02-06 18:46:49 -0600

    workstation-wazuh.sh
    Workstation Wazuh install agent

---
+++ workstation-wazuh.sh
@@ -0,0 +1,1 @@
+wget https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.7.2-1_amd64.deb && sudo WAZUH_MANAGER='172.20.242.10' WAZUH_AGENT_NAME='Workstation' dpkg -i ./wazuh-agent_4.7.2-1_amd64.deb

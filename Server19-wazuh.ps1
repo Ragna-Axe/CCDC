87c3d8fbe66f91e1722d9cf6b9b032c44490a436
Ragna-Axe <147952264+Ragna-Axe@users.noreply.github.com>
2024-02-06 18:58:03 -0600

    Server19-wazuh.ps1
    wazuh agent install

---
+++ Server19-wazuh.ps1
@@ -0,0 +1,1 @@
+Invoke-WebRequest -Uri https://packages.wazuh.com/4.x/windows/wazuh-agent-4.7.2-1.msi -OutFile ${env.tmp}\wazuh-agent; msiexec.exe /i ${env.tmp}\wazuh-agent /q WAZUH_MANAGER='172.20.242.10' WAZUH_AGENT_NAME='Server19' WAZUH_REGISTRATION_SERVER='172.20.242.10' 

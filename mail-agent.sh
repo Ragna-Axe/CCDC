094fe2244efeeb32de174ffc746e933da1d83561
Ragna-Axe <147952264+Ragna-Axe@users.noreply.github.com>
2024-02-06 20:00:01 -0600

    Create mail-agent.sh
    Wazuh agent

---
+++ mail-agent.sh
@@ -0,0 +1,16 @@
+#agent install wazuh
+
+curl -o wazuh-agent-4.7.2-1.x86_64.rpm https://packages.wazuh.com/4.x/yum/wazuh-agent-4.7.2-1.x86_64.rpm && sudo WAZUH_MANAGER='172.20.242.10' WAZUH_AGENT_NAME='Mail' rpm -ihv wazuh-agent-4.7.2-1.x86_64.rpm
+
+#!/bin/bash
+
+# Reload systemd
+sudo systemctl daemon-reload
+
+# Enable Wazuh agent service
+sudo systemctl enable wazuh-agent
+
+# Start Wazuh agent service
+sudo systemctl start wazuh-agent
+
+echo "Systemd commands executed successfully!"
